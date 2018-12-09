5299b00a81921c4ff1927c55575a9cb500c24900
Aishwarya Yeole <aishwarya.yeole@xoriant.com>
2018-12-09 18:42:54 +0530

    second commit

--- level2-complete/functions/index.js
+++ level2-complete/functions/index.js
@@ -1,32 +1,12 @@
-// Copyright 2018, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS IS' BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 "use strict";
-
-// Import the Dialogflow module and response creation dependencies
-// from the Actions on Google client library.
 const { dialogflow, Suggestions } = require("actions-on-google");
-
-// Import the firebase-functions package for deployment.
 const functions = require("firebase-functions");
-
-// Instantiate the Dialogflow client.
 const app = dialogflow({ debug: true });
+const {
+  searchMeetingRoom,
+  bookMeetingRoom
+} = require("./meeting_room/meetingRoomController");
 
-const meeting_rooms = ["Jogi", "Kafi", "Basant", "Asavari"];
-
-function searchMeetingRoom(person_count, date, duration, time) {
-  return meeting_rooms;
-}
 app.intent(
   "search meeting room",
   (conv, { person_count, date, duration, time }) => {
@@ -35,19 +15,40 @@ app.intent(
     conv.user.storage = {
       pc: person_count,
       date: date,
-      duration: duration,
+      duration: duration.amount,
       time: time
     };
-    conv.add("Please select your meeting room ");
-    conv.ask(new Suggestions(["Jogi", "Kafi", "Basant", "Asavari"]));
+    if (rooms.length > 0) {
+      conv.add("Please select your meeting room ");
+      const room_names = rooms.map(r => r.name);
+      conv.ask(new Suggestions(room_names));
+    } else {
+      conv.close("Ghar ja");
+    }
   }
 );
 
 app.intent("search meeting room - book", (conv, { meeting_room }) => {
-  conv.close(
-    "You selected " + meeting_room + " for " + conv.user.storage.pc + " people"
-  );
+  if (
+    bookMeetingRoom(
+      meeting_room,
+      conv.user.storage.time,
+      conv.user.storage.duration
+    )
+  ) {
+    conv.ask(
+      "Great, your meeting room has been booked. bla bla, Would you like to do anything else?"
+    );
+  } else {
+    conv.ask(
+      "Sorry, the room could not be booked, would you like to do something else?"
+    );
+  }
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+/*********** test ************* */
+
+bookMeetingRoom("Jogiya", "2018-12-09T12:45:02.391Z", 4);
